03a0844387a32230f6c142fe45310062e73b32a0
thiskartik <gautam.kartik05@gmail.com>
2020-08-03 16:50:08 +0530

    added reply functionality

--- telegrambot.py
+++ telegrambot.py
@@ -1,8 +1,15 @@
 import requests
 import json
+import configparser as cfg
 
-token = "<YOUR BOTS ID HERE>"
-chat = "@testing0694"
+token = ""
+chat =""
+
+def get_token():
+    parser = cfg.ConfigParser()
+    parser.read("config.cfg")
+    token = parser.get('creds','token')
+    return token
 
 one = "http://api.telegram.org/bot"
 two = "/sendMessage?chat_id="
@@ -14,7 +21,7 @@ time = "&timeout="
 
 # Final URL
 apiUrl = one+token+two+chat+three
-getUpdates = one+token+"/getUpdates"
+getUpdates = one+get_token()+"/getUpdates"
 
 # Functions
 def sendMessage(msg):
@@ -32,16 +39,49 @@ def getdataId(id):
     return json.loads(res)
 
 def getdataPool(id,sec):
+    print(token)
     res = requests.get(getUpdates+offset+id+time+sec).text
     return json.loads(res)
 
+def sendMsg(msg,cId):
+    res = requests.get(one+token+"/sendMessage?text="+msg+"&chat_id="+cId)
+    print(res)
+
+def checkMsg(msg):
+    greets = ["Hi","Hello"]
+    names = ["name","Name"]
+    if searchStr(greets,msg):
+        return "Hello! How Can i help ?"
+    elif searchStr(names,msg):
+        return "My name is Hyperclite bot. What is yours ?"
+    else:
+        return "Sorry Cant help"
+
+def searchStr(lst,key):
+    for i in lst:
+        if key.find(i)!=-1:
+            return 1                
+
 ## Main Code
-startId = 701287669
-for i in range(0,100):
+# startId = 701287669
+# for i in range(0,100):
+#     startId = startId+1
+#     print("running at - "+str(startId))
+#     res = getdataPool(str(startId),"5")
+#     if res['result']!=[]:
+#         print(res['result'][0]['message']['text']+" : "+str(res['result'][0]['update_id']))
+#     if res['result'][0]['message']['text']=='/send':
+#         sendMessage("test")
+
+startId = 701287715
+while True:
+    get_token()
     startId = startId+1
-    print("running at - "+str(startId))
-    res = getdataPool(str(startId),"5")
-    if res['result']!=[]:
+    res = getdataPool(str(startId),"50")
+    print(res)
+    if res['ok']==False:
+        print("Error")
+        print(res)
+    elif res['result']!=[]:
         print(res['result'][0]['message']['text']+" : "+str(res['result'][0]['update_id']))
-    if res['result'][0]['message']['text']=='/send':
-        sendMessage("test")
+    sendMsg(checkMsg(res['result'][0]['message']['text']),str(res['result'][0]['message']['chat']['id']))        
